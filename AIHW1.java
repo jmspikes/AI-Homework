5acbb310c6e74505dd0783e15e50fde9bee79a00
Jonathan Spikes <jmspikes@uark.edu>
2018-09-03 19:34:35 -0500

    Add files via upload

--- AIHW1.java
+++ AIHW1.java
@@ -1,85 +1,169 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Random;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.TreeSet;
 
+class Main {
 
-class Board{
+	public static void main(String[] args) {
 
-	boolean[][] area;
-	byte[] state;
-	Main prev;
-	int counter;
-	int drawnBlocks;
-	boolean goalReached = false;
-
-	Board(Main p){
-		//populates board with black spaces
-		prev = p;
-		area = new boolean[10][10];
-		state = new byte[22];
-		counter = 0;
-		drawnBlocks = 0;
-	}
-
-	public void b(int x, int y){
-		if(this.area[x][y] == false)
-			this.counter++;
-		this.area[x][y] = true;
-		this.drawnBlocks++;
-	}
-
-		// Draw a 3-block piece
-	public void shape(int id, int x1, int y1, int x2, int y2, int x3, int y3)
-	{
-		b(state[2 * id] + x1, state[2 * id + 1] + y1);
-		b(state[2 * id] + x2, state[2 * id + 1] + y2);
-		b(state[2 * id] + x3, state[2 * id + 1] + y3);
-		//reset places we're no longer at to false
-	}
-
-	// Draw a 4-block piece
-	public void shape(int id, int x1, int y1, int x2, int y2,
-		int x3, int y3, int x4, int y4)
-	{
+		Board initial = new Board(null);
+		
+		initial.drawBoard();
+		//initial.printBoard();
+		
+		BFS breadth = new BFS(initial);
 	
-		shape(id, x1, y1, x2, y2, x3, y3);
-		b(state[2 * id] + x4, state[2 * id + 1] + y4);
+		System.exit(0);
 	}
+	
+}
 
-	public void tryMove(int id, int direction){
+class BFS{
+	
+	Queue<Board> queue;
+	TreeSet<Board> visited;
+	StateComparator comp = new StateComparator();
+	Board finalBoard;
+	Board vertex;
+	Movement move;
+	ArrayList<byte[]> route;
+	boolean isFound = false;
+	int moveCount = 0;
 
-		Board alternate = new Board(this.prev);
-		for(int i = 0; i < alternate.state.length; i++){
-				alternate.state[i] = this.state[i];
+	BFS(Board b){
+		queue =  new LinkedList<Board>();
+		route = new ArrayList<byte[]>();
+		visited = new TreeSet<Board>(comp);
+		queue.add(b);
+		visited.add(b);
+		
+		while(!(queue.isEmpty()) && isFound == false){
+			
+			vertex = queue.poll();
+
+			Board cV = copy(vertex);
+			
+				//find all neighbors
+				for(int pos = 0; pos < 22; pos++){
+					move = new Movement(cV);
+					move.tryMove(pos, 1);
+					move.tryMove(pos, -1);
+					for(int i = 0; i < move.ret.size(); i++){
+						
+						if(!(visited.contains(move.ret.get(i)))){
+							queue.add(move.ret.get(i));
+							visited.add(move.ret.get(i));
+							if(move.ret.get(i).state[1] == -2){
+								isFound = true;
+								finalBoard = move.ret.get(i);
+							}
+						}
+					}
+				}
 		}
-		alternate.area = new boolean[10][10];
 
-		alternate.state[id]+=direction;
-		alternate.generate();
-		if(alternate.counter == this.drawnBlocks){
-			System.out.println("State valid");
-			this.state[id]+=direction;
-			if(this.state[1] == -2){
-				System.out.println("Goal reached");
-				goalReached = true;
+		boolean go = true;
+		while(go){
+			
+			route.add(finalBoard.state);
+			moveCount++;
+			finalBoard.printBoard();
+			if(finalBoard.parent == null){
+				go = false;
 			}
 			else
-				System.out.println("Goal not reached");
+				finalBoard = finalBoard.parent;
+			
 		}
-		else
-			System.out.println("Invalid state");
+		
+		byte[] by;
+		for(int i = 0; i < route.size(); i++){
+			 by = route.get(i);
+			 for(int j = 0; j < by.length; j++)
+				 System.out.print(by[j]+ " ");
+			 System.out.println();
+		}
+		System.out.println("Move Count: " + moveCount);
 
 	}
 	
-	public int generateMove(){
+	public Board copy(Board item){
 		
+		Board c;
 		
-		return 0;
+		c = new Board(null);
+		for(int i = 0; i < item.state.length; i++)
+			c.state[i] = item.state[i];
+		
+		c.drawnBlocks = item.drawnBlocks;
+		c.takenSpaces = item.takenSpaces;
+		c.parent = item.parent;
+		
+		for(int i = 0; i < item.board.length; i++)
+			for(int j = 0; j < item.board.length; j++)
+				c.board[j][i] = item.board[j][i];
+		
+		return c;
 	}
+	
+}
 
-	void generate(){
+class Movement{
+	
+	Board toCheck;
+	ArrayList<Board> ret;
+	Movement(Board vertex){
+		
+		toCheck = vertex;
+		ret = new ArrayList<Board>();
+	}
+	
+	public void tryMove(int id, int direction){
+		
+		//create new board to check, parent is board to check from
+		Board t = new Board(toCheck);
+	
+		t.state[id] +=direction;
+		t.drawBoard();
+//		t.printBoard();
+		
+		//if the board move is valid, return it to be added to valid moves queue
+		if(t.isStateValid(t)){
+//			System.out.println("valid");
+			ret.add(t);}
+//		else 
+//			System.out.println("not valid");
+	}
+	
+}
 
+class Board {
+	
+	boolean[][] board;
+	byte[] state;
+	Board parent;
+	int takenSpaces;
+	int drawnBlocks;
+	
+	Board(Board b){
+		
+		state = new byte[22];
+		if(b != null)
+			for(int i = 0; i < state.length; i++)
+				state[i] = b.state[i];
+		
+		board = new boolean[10][10];
+		parent = b;
+		takenSpaces = 0;
+		drawnBlocks = 0;
+		
+	}
+	
+	public void drawBoard(){
+	
 		for(int i = 0; i < 10; i++) { this.b(i, 0); this.b(i, 9); }
 		for(int i = 1; i < 9; i++) { this.b(0, i); this.b(9, i); }
 		b(1, 1); b(1, 2); b(2, 1);
@@ -87,31 +171,57 @@ class Board{
 		b(1, 7); b(1, 8); b(2, 8);
 		b(8, 7); b(7, 8); b(8, 8);
 		b(3, 4); b(4, 4); b(4, 3);
-
 		
 		//red block
-		//game.area[3][1] = game.area[3][2] = game.area[4][1] = game.area[4][2] = true;
-		//red block
-		shape(0, 1, 3, 2, 3, 1, 4, 2, 4);/*
+		shape(0, 1, 3, 2, 3, 1, 4, 2, 4);
+		//green block
 		shape(1, 1, 5, 1, 6, 2, 6);
-		game.shape(2,2, 5, 3, 5, 3, 6);
-		game.shape(3, 3, 7, 3, 8, 4, 8);
-		game.shape(4, 4, 7, 5, 7, 5, 8);
-		game.shape(5, 6, 7, 7, 7, 6, 8);
-		game.shape(6, 5, 4, 5, 5, 5, 6, 4, 5);
-		game.shape(7, 6, 4, 6, 5, 6, 6, 7, 5);
-		game.shape(8, 8, 5, 8, 6, 7, 6);
-		game.shape(9, 6, 2, 6, 3, 5, 3);
-		game.shape(10, 5, 1, 6, 1, 5, 2);*/
-		printBoard();
+		//navy block
+		shape(2, 2, 5, 3, 5, 3, 6);
+		//pink block
+		shape(3, 3, 7, 3, 8, 4, 8);
+		//yellow block
+		shape(4, 4, 7, 5, 7, 5, 8);
+		//tan block
+		shape(5, 6, 7, 7, 7, 6, 8);
+		//skyblue block
+		shape(6, 5, 4, 5, 5, 5, 6, 4, 5);
+		//green block
+		shape(7, 6, 4, 6, 5, 6, 6, 7, 5);
+		shape(8, 8, 5, 8, 6, 7, 6);
+		shape(9, 6, 2, 6, 3, 5, 3);
+		shape(10,5, 1, 6, 1, 5, 2);
+		}
+	
+	public void b(int x, int y){
+		//if the spaces is taken it wont be counted, if takenSpaces != drawnBlocks then we have drawn over
+		//a block making the state invalid
+		if(this.board[x][y] == false){
+			this.takenSpaces++;
+		}
+		this.board[x][y] = true;
+		this.drawnBlocks++;	
 	}
-
-
+	//mgashler code	
+	public void shape(int id, int x1, int y1, int x2, int y2, int x3, int y3)
+	{
+		b(state[2 * id] + x1, state[2 * id + 1] + y1);
+		b(state[2 * id] + x2, state[2 * id + 1] + y2);
+		b(state[2 * id] + x3, state[2 * id + 1] + y3);
+	}
+	//mgashler code
+	public void shape(int id, int x1, int y1, int x2, int y2,
+		int x3, int y3, int x4, int y4)
+	{
+		shape(id, x1, y1, x2, y2, x3, y3);
+		b(state[2 * id] + x4, state[2 * id + 1] + y4);
+	}
+	
 	void printBoard(){
 
-		for(int i = 0; i < area.length; i++){
-			for(int j = 0; j < area.length; j++){
-				if(area[j][i]){
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[j][i]){
 					System.out.print("[-]");
 				}
 				else
@@ -119,12 +229,19 @@ class Board{
 			}
 			System.out.println();
 		}
-	System.out.println("-----------------------------------------------------------");
+		System.out.println();
 	}
-
+	
+	public boolean isStateValid(Board check){	
+		//if drawn blocks is equal to the amount of spaces used then the state is valid
+		if(check.drawnBlocks == check.takenSpaces)
+			return true;
+		return false;
+	}	
 }
 
-class StateComparator implements Comparator<Board>{
+class StateComparator implements Comparator<Board>
+{
 	public int compare(Board a, Board b)
 	{
 		for(int i = 0; i < 22; i++)
@@ -138,40 +255,270 @@ class StateComparator implements Comparator<Board>{
 	}
 }
 
+/*package bfs;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.TreeSet;
 
-class Main{
+class Main {
+
+	public static void main(String[] args) {
+
+		Board initial = new Board(null);
+		
+		//initial.drawBoard();
+		initial.printBoard();
+		
+		BFS breadth = new BFS(initial);
 	
-	Board game = new Board(this);
-	TreeSet<Board> set;
-	StateComparator comp;
-
-	public Main(){
-
-		game = new Board(this);
-		StateComparator comp = new StateComparator();
-		set = new TreeSet<Board>(comp);
-		driver();
+		System.exit(0);
 	}
 	
-	public void driver(){
-		game.generate();
-		game.tryMove(1, 1);
-		game.tryMove(1,1);
-		game.tryMove(0, 1);
-		game.tryMove(0, 1);
-		game.tryMove(0, 1);
-		game.tryMove(0, 1);
-		game.tryMove(1, -1);
-		game.tryMove(1, -1);
-		game.tryMove(1, -1);
-		game.tryMove(1, -1);
-	}
-
-	public static void main(String[] args){
-
-		new Main();
-	}
-
-
 }
+
+class BFS{
+	
+	Queue<Board> queue;
+	TreeSet<Board> visited;
+	StateComparator comp = new StateComparator();
+	Board finalBoard;
+	Board vertex;
+	Movement move;
+	ArrayList<byte[]> route;
+	boolean isFound = false;
+	int moveCount = 0;
+
+	BFS(Board b){
+		queue =  new LinkedList<Board>();
+		route = new ArrayList<byte[]>();
+		visited = new TreeSet<Board>(comp);
+		queue.add(b);
+		visited.add(b);
+		
+		while(!(queue.isEmpty()) && isFound == false){
+			
+			vertex = queue.poll();
+
+			Board cV = new Board(vertex);
+			
+				//find all neighbors
+				for(int pos = 0; pos < 2; pos++){
+					move = new Movement(cV);
+					move.tryMove(pos, 1);
+					move.tryMove(pos, -1);
+					for(int i = 0; i < move.ret.size(); i++){
+						
+						if(!(visited.contains(move.ret.get(i)))){
+							queue.add(move.ret.get(i));
+							visited.add(move.ret.get(i));
+							if(move.ret.get(i).state[1] == -2){
+								isFound = true;
+								finalBoard = move.ret.get(i);
+							}
+						}
+					}
+				}
+		}
+
+		boolean go = true;
+		while(go){
+			
+			route.add(finalBoard.state);
+			moveCount++;
+			if(finalBoard.parent == null){
+				go = false;
+			}
+			else{
+				finalBoard.printBoard();
+				finalBoard = finalBoard.parent;
+			}
+			
+		}
+		
+		byte[] by;
+		for(int i = 0; i < route.size(); i++){
+			 by = route.get(i);
+			 for(int j = 0; j < by.length; j++)
+				 System.out.print(by[j]+ " ");
+			 System.out.println();
+		}
+		System.out.println("Move Count: " + moveCount);
+
+	}
+	
+	/*
+	public Board copy(Board item){
+		
+		Board c;
+		
+		c = new Board(item);
+	
+		for(int i = 0; i < item.state.length; i++)
+			c.state[i] = item.state[i];
+		
+		c.drawnBlocks = item.drawnBlocks;
+		c.takenSpaces = item.takenSpaces;
+		c.parent = item.parent;
+		
+		for(int i = 0; i < item.board.length; i++)
+			for(int j = 0; j < item.board.length; j++)
+				c.board[j][i] = item.board[j][i];
+		
+		return c;
+	}
+	
+}
+
+class Movement{
+	
+	Board toCheck;
+	ArrayList<Board> ret;
+	Movement(Board vertex){
+		
+		toCheck = vertex;
+		ret = new ArrayList<Board>();
+	}
+	
+	public void tryMove(int id, int direction){
+		
+		//create new board to check, parent is board to check from
+		//Board t = new Board(toCheck);
+	
+		toCheck.state[id] +=direction;
+		toCheck.drawBoard();
+		toCheck.printBoard();
+		
+		//if the board move is valid, return it to be added to valid moves queue
+		if(toCheck.isStateValid(toCheck)){
+//			System.out.println("valid");
+			ret.add(toCheck);}
+//		else 
+//			System.out.println("not valid");
+	}
+	
+}
+
+class Board {
+	
+	boolean[][] board;
+	byte[] state;
+	Board parent;
+	int takenSpaces;
+	int drawnBlocks;
+	
+	Board(Board bP){
+		
+		state = new byte[22];
+		if(bP != null){
+			for(int i = 0; i < state.length; i++)
+				state[i] = bP.state[i];
+			takenSpaces = bP.takenSpaces;
+			drawnBlocks = bP.drawnBlocks;
+			
+		}
+		
+		//board = new boolean[10][10];
+		if(bP == null){
+			board = new boolean[10][10];
+			for(int i = 0; i < 10; i++) { this.b(i, 0); this.b(i, 9); }
+			for(int i = 1; i < 9; i++) { this.b(0, i); this.b(9, i); }
+			b(1, 1); b(1, 2); b(2, 1);
+			b(7, 1); b(8, 1); b(8, 2);
+			b(1, 7); b(1, 8); b(2, 8);
+			b(8, 7); b(7, 8); b(8, 8);
+			b(3, 4); b(4, 4); b(4, 3);
+		}
+		else	
+			board = bP.board;
+		parent = bP;
+
+		
+	}
+	
+	public void drawBoard(){
+
+		
+		//red block
+		shape(0, 1, 3, 2, 3, 1, 4, 2, 4);/*
+		//green block
+		shape(1, 1, 5, 1, 6, 2, 6);
+		//navy block
+		shape(2, 2, 5, 3, 5, 3, 6);
+		//pink block
+		shape(3, 3, 7, 3, 8, 4, 8);
+		//yellow block
+		shape(4, 4, 7, 5, 7, 5, 8);
+		//tan block
+		shape(5, 6, 7, 7, 7, 6, 8);
+		//skyblue block
+		shape(6, 5, 4, 5, 5, 5, 6, 4, 5);
+		//green block
+		shape(7, 6, 4, 6, 5, 6, 6, 7, 5);
+		}
+	
+	public void b(int x, int y){
+		//if the spaces is taken it wont be counted, if takenSpaces != drawnBlocks then we have drawn over
+		//a block making the state invalid
+		if(this.board[x][y] == false){
+			this.takenSpaces++;
+		}
+		this.board[x][y] = true;
+		this.drawnBlocks++;	
+	}
+	//mgashler code	
+	public void shape(int id, int x1, int y1, int x2, int y2, int x3, int y3)
+	{
+		b(state[2 * id] + x1, state[2 * id + 1] + y1);
+		b(state[2 * id] + x2, state[2 * id + 1] + y2);
+		b(state[2 * id] + x3, state[2 * id + 1] + y3);
+	}
+	//mgashler code
+	public void shape(int id, int x1, int y1, int x2, int y2,
+		int x3, int y3, int x4, int y4)
+	{
+		shape(id, x1, y1, x2, y2, x3, y3);
+		b(state[2 * id] + x4, state[2 * id + 1] + y4);
+	}
+	
+	void printBoard(){
+
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[j][i]){
+					System.out.print("[-]");
+				}
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public boolean isStateValid(Board check){	
+		//if drawn blocks is equal to the amount of spaces used then the state is valid
+		if(check.drawnBlocks == check.takenSpaces)
+			return true;
+		return false;
+	}	
+}
+
+class StateComparator implements Comparator<Board>
+{
+	public int compare(Board a, Board b)
+	{
+		for(int i = 0; i < 22; i++)
+		{
+			if(a.state[i] < b.state[i])
+				return -1;
+			else if(a.state[i] > b.state[i])
+				return 1;
+		}
+		return 0;
+	}
+}
+*/
