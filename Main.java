d96b14c5961098d4543a1586ecdc0bef82d46926
Jonathan Spikes <jmspikes@uark.edu>
2018-11-26 20:06:47 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-package Homework7;
-
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,129 +8,274 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		int counter = 0;
 		Board b = new Board();
-		
-		for(int i = 0; i < 10; i++){
-			for(int j = 0; j < 20; j++){
-				System.out.print(b.scoreBoard[i][j][0] + " ");
+
+		while(true){
+			counter++;
+			if(b.y == 0 && b.x == b.scoreBoard[0].length-1){
+				b.y = b.scoreBoard.length-1;
+				b.x = 0;
+				b.scoreBoard[0][b.scoreBoard[0].length-1] = b.reward;
+				b.displayBoard[0][b.scoreBoard[0].length-1] = "G";
 			}
-			System.out.println();
-		}
-		
-		for(int i = 0; i < 10; i++){
-			for(int j = 0; j < 20; j++){
-				System.out.print(b.displayBoard[i][j][0]);
+
+			b.learner();
+			
+			if(counter % 168559772*18 == 0){
+				b.print();
+				System.exit(0);		
 			}
-			System.out.println();
+				
+		
 		}
 	}
 
-	//prints whatever object is passed to it
-	public static void print(Object o){
-		System.out.println(o.toString());
-	}
-	
 }
 
 
 class Board{
 	
-	double[][][] scoreBoard;
-	String[][][] displayBoard;
-	int currentX;
-	int currentY;
+	Double[][] scoreBoard;
+	String[][] displayBoard;
+	int[] moves;
 	Random rand = null;
-	double epsilon = 0.05;
-	int action;
-	int x;
+	double epsilon = 0.03;
 	int y;
+	int x;
+	int moveX;
+	int moveY;
+	int bX = 20;
+	int bY = 10;
+	static double reward = 1;
+	static double penalty = -1;
+	ArrayList<Integer> validMoves;
+	int prevAction;
+	double baseTileAmount = 0.0;
+
 	
 	Board(){
 		
 		//creates boards
-		scoreBoard = new double[10][20][4];
-		displayBoard = new String[10][20][4];
+		scoreBoard = new Double[bY][bX];
+		displayBoard = new String[bY][bX];
 		rand = new Random();
-
+		prevAction = -1;
 		
-		for(int i = 0; i < 10; i++){
-			for(int j = 0; j < 20; j++){
-				scoreBoard[i][j][0] = -1;
-				displayBoard[i][j][0] = " ";
+		for(int i = 0; i < scoreBoard.length; i++){
+			for(int j = 0; j < scoreBoard[0].length; j++){
+				scoreBoard[i][j] = baseTileAmount;
+				displayBoard[i][j] = " ";
 			}
 		}
 		
 		//sets initial pieces
 		//player
-		scoreBoard[9][0][0] = 1;
-		displayBoard[9][0][0] = "S";
+		displayBoard[bY-1][0] = "S";
 		//goal
-		scoreBoard[0][19][0] = 99;
-		displayBoard[0][19][0] = "G";
+		scoreBoard[0][bX-1] = reward;
+		displayBoard[0][bX-1] = "G";
 		//roadblocks
-		for(int i = 0; i < 10; i++){
-			if(i != 5 && i != 4){
-			scoreBoard[i][9][0] = -99;
-			displayBoard[i][9][0] = "#";
+		for(int i = 0; i < scoreBoard.length; i++){
+			if(i != scoreBoard.length/2-1 && i != scoreBoard.length/2){
+			scoreBoard[i][scoreBoard[0].length/2-1] = penalty;
+			displayBoard[i][scoreBoard[0].length/2-1] = "#";
 			}
 		}
+		y = scoreBoard.length-1;
 		x = 0;
-		y = 0;
+		
 	}
 	
 	void learner() {
 		
+		int xOrig = x;
+		int yOrig = y;
 		
-		if(rand.nextDouble() < epsilon) {
-			action = rand.nextInt(4);
-		}
+		//returns an action to move
+		int action = 0;
+		action = getAction(action);
+		//avoids going back and forth
+		if(prevAction != -1)
+			while(true){
+				if((action == 1 && prevAction == 0) || (action == 0 && prevAction == 1) || (action == 3 && prevAction == 2) || (action == 2 && prevAction == 3) )
+					action = getAction(action);
+				else
+					break;
+
+			}
+		prevAction = action; 
+
+		doAction(action);
+
 		
-		else {
-			
-			action = 0;
-			for(int candidate = 0; candidate < 4; candidate++) {
-				if(scoreBoard[x][y][candidate] > scoreBoard[x][y][action])
-					action = candidate;
-				
-				if(scoreBoard[x][y][action] == 0.0)
-					action = rand.nextInt(4);		
-			}
-			
-			doAction(action);
-			int[] j = new int[2];
-			j[0] = x;
-			j[1] = y;
-			
-			int j_action = 0; 
-			
-			for(int candidate = 0; candidate < 4; candidate++) {
-				
-				if(scoreBoard[x][y][candidate] > scoreBoard[x][y][action])
-					j_action = candidate;
-				
-				if(scoreBoard[x][y][action] == 0.0)
-					j_action = rand.nextInt(4);		
-				
-			}
-			
-			double alphaK = 0.1;
-			double gamma = 0.97;
-			
-			scoreBoard[x][y][action] = (1 - alphaK) * scoreBoard[x][y][action] + alphaK * (reward(scoreBoard, x, y, action) +  gamma*scoreBoard[x][y][j_action]);			
-			
-		}
+		int xX = x;
+		int xY = y;
+		
+		int j_action = 0;
+		j_action = getAction(j_action);
+		doAction(j_action);
+		
+		int jX = x;
+		int jY = y;
+
+		double alphaK = 0.1;
+		double gamma = 0.97;
+		double amount = ((1 - alphaK) * scoreBoard[xY][xX]) + (alphaK * (scoreBoard[jY][jX] + (gamma*maxQ(jY,jX))));	
+
+		scoreBoard[jY][jX] = amount;
+		
+		moveDisplayBoard(xY, xX, j_action);
+		x = xX;
+		y = xY;
+		
 	}
 	
-	double reward(double[][][] scoreBoard, int x, int y, int action) {
+	void moveDisplayBoard(int y, int x, int action){
 		
-		double r = 0;
+		if(action == 0)
+			displayBoard[y][x] = ">";
+		if(action == 1)
+			displayBoard[y][x] = "<";
+		if(action == 2)
+			displayBoard[y][x] = "V";
+		if(action == 3)
+			displayBoard[y][x] = "^";
+			
+		
+	}
+	
+	int getAction(int action){
 		
 		
-		return r;
+		boolean[] moves = new boolean[4];
+		
+		
+		//right
+		x++;
+		moves[0] = x < scoreBoard[0].length && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		x--;
+		//left
+		x--;
+		moves[1] = x >= 0 && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		x++;
+		//down
+		y++;
+		moves[2] = y < scoreBoard.length && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		y--;
+		//up
+		y--;
+		moves[3] = y >= 0 && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		y++;
+		
+		
+		validMoves = new ArrayList<Integer>();
+		
+		//adds each valid move to an array list, numbering scheme means whichever i we're at coresponds to r-l-d-u
+		//just need to check array list value to tell which move is which
+		for(int i = 0; i < 4; i++)
+			if(moves[i] == true)
+				validMoves.add(i);
+		
+		//gets random valid move
+		if(rand.nextDouble() < epsilon){ 
+			double amount;
+			ArrayList<Double> toRemove = new ArrayList<Double>();
+			for(int i = 0; i < validMoves.size(); i++){
+			int testX = (int) (x + (amount = validMoves.get(i) == 0 ? 1 : (validMoves.get(i) == 1 ? -1 : 0)));
+			int testY = (int) (y + (amount = validMoves.get(i) == 2 ? 1 : (validMoves.get(i) == 3 ? -1 : 0)));
+			
+			amount = getScoreOfLocation(testY, testX);
+			toRemove.add(amount);
+			}
+			for(int i = 0; i < toRemove.size(); i++){
+				if(toRemove.get(i) == penalty)
+					validMoves.remove(i);
+			}
+			action = validMoves.get(rand.nextInt(validMoves.size()));
+		}
+		else{
+			
+			double max = -1000;
+			double amount;
+			action = validMoves.get(0);
+			ArrayList<Double> toRemove = new ArrayList<Double>();
+			//will loop over all valid moves and get the best move
+			for(int i = 0; i < validMoves.size(); i++){
+				int testX = (int) (x + (amount = validMoves.get(i) == 0 ? 1 : (validMoves.get(i) == 1 ? -1 : 0)));
+				int testY = (int) (y + (amount = validMoves.get(i) == 2 ? 1 : (validMoves.get(i) == 3 ? -1 : 0)));
+				
+				amount = getScoreOfLocation(testY, testX);
+				toRemove.add(amount);
+				if(amount > max){
+					max = amount;
+					action = validMoves.get(i);
+				}
+			}
+			
+			//check to make sure we dont just have base tiles
+			if(max == baseTileAmount){
+				ArrayList<Integer> finalists = new ArrayList<Integer>();
+				for(int i = 0; i < toRemove.size(); i++){
+					if(toRemove.get(i) >= max)
+						finalists.add(validMoves.get(i));
+				}
+				int chance = finalists.get(rand.nextInt(finalists.size()));
+				action = chance;	
+			}
+			
+		}
+		
+		return action;
+	}
+	
+	
+	double getScoreOfLocation(int y, int x){	
+		return scoreBoard[y][x];
+	}
+	
+	
+	double maxQ(int y, int x) {
+	
+		double max = 0;
+		boolean[] moves = new boolean[4];
+		
+		//right
+		x++;
+		moves[0] = x < scoreBoard[0].length && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		x--;
+		//left
+		x--;
+		moves[1] = x >= 0 && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		x++;
+		//down
+		y++;
+		moves[2] = y < scoreBoard.length && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		y--;
+		//up
+		y--;
+		moves[3] = y >= 0 && !(x == 0 && y == scoreBoard.length-1) ? true : false;
+		y++;
+		
+		
+		double amount;
+		//adds each valid move to an array list, numbering scheme means whichever i we're at coresponds to r-l-u-d
+		//just need to check array list value to tell which move is which
+		for(int i = 0; i < 4; i++)
+			if(moves[i] == true){
+
+				int testX = (int) (x + (amount = i == 0 ? 1 : (i == 1 ? -1 : 0)));
+				int testY = (int) (y + (amount = i == 2 ? 1 : (i == 3 ? -1 : 0)));
+				
+				amount = getScoreOfLocation(testY, testX);
+				if(amount > max)
+					max = amount;
+			}
+
+		return max;
 	}
 	
 	void doAction(int action) {
-	
 		if(action == 0)
 			x++;
 		if(action == 1)
@@ -138,7 +284,22 @@ class Board{
 			y++;
 		if(action == 3)
 			y--;
-		
-		
 	}
+	
+	public void print(){
+		for(int i = 0; i < scoreBoard.length; i++){
+			for(int j = 0; j < scoreBoard[0].length; j++){
+					System.out.print(scoreBoard[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 0; i < scoreBoard.length; i++){
+			for(int j = 0; j < scoreBoard[0].length; j++){
+				System.out.print(displayBoard[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
 }
